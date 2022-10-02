e4a5a4a67f839e71721401ea152f863daa35da76
Juan Gorosito <jpablo.gorosito87@gmail.com>
2022-10-02 03:59:46 -0300

    Origin

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
+
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+
+  &:hover {
+    background-color: #7a7bfe;
+    cursor: pointer;
+  }
+`;
+
+const Formulario = () => {
+  const [moneda,SelectMonedas] = useSelectMonedas("Selecciona tu moneda", monedas);
+
+  return (
+    <form action="">
+      <SelectMonedas
+      />
+      <InputSubmit type="submit" value="Cotizar" />
+    </form>
+  );
+};
+
+export default Formulario;
---
+++ src/data/monedas.js
@@ -0,0 +1,9 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    {id: 'ARG', nombre: 'Peso Argentino'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'GBP', nombre: 'Libra Esterlina'}
+]
+export{
+    monedas
+}
